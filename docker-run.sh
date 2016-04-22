6c9403a5214183ce1c690598439264ad74dd8e7c
k4zzk <kosuke19952000@gmail.com>
2016-04-22 20:11:11 +0900

    Update docker run script

---
+++ docker-run.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# from http://qiita.com/yudoufu/items/48cb6fb71e5b498b2532
+scriptDir=$(cd $(dirname ${BASH_SOURCE:-$0}); pwd)
+
+docker run -d -p 80:80 \
+-v $scriptDir/conf.d:/etc/nginx/conf.d \
+-v $scriptDir/certs:/etc/nginx/certs \
+-v $scriptDir/log:/var/log/nginx \
+-v $scriptDir/www:/var/www/html \
+k4zzk/nginx
