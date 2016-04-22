33f12b803b6ddd293a48090fc1b3f1afd32e7ac3
k4zzk <kosuke19952000@gmail.com>
2016-04-23 03:13:16 +0900

    Update docker-run.sh

--- docker-run.sh
+++ docker-run.sh
@@ -3,7 +3,7 @@
 # from http://qiita.com/yudoufu/items/48cb6fb71e5b498b2532
 scriptDir=$(cd $(dirname ${BASH_SOURCE:-$0}); pwd)
 
-docker run -d -p 80:80 \
+docker run --privileged -d -p 80:80 \
 -v $scriptDir/conf.d:/etc/nginx/conf.d \
 -v $scriptDir/certs:/etc/nginx/certs \
 -v $scriptDir/log:/var/log/nginx \
