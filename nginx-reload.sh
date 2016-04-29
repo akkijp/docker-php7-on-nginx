2b0419c0e123951e2a6ecc529ee153f9ab70dd8e
Kosuke Akizuki <k4zzk@users.noreply.github.com>
2016-04-29 19:33:17 +0900

    Update nginx-reload.sh

--- nginx-reload.sh
+++ nginx-reload.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
-pid=$(cat docker-nginx.pid)
+if [ -f docker-nginx.pid ]; then
+  pid=$(cat docker-nginx.pid)
+else
+  echo "pid file Not foud!" >&2
+  exit -1
+fi
 
 docker exec -i $pid /usr/sbin/nginx -t &&
 docker exec -i $pid /usr/sbin/service nginx reload
